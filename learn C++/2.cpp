7917cc2fabb1fe629288b900afc6772254651fd7
Shkityrk <81920594+Shkityrk@users.noreply.github.com>
2022-04-18 19:25:49 +0300

    ураааа!

--- learn C++/2.cpp
+++ learn C++/2.cpp
@@ -1,8 +1,10 @@
     #include <iostream>
+    #include <conio.h>
     using namespace std;
     int main()
     {
         int a,b;
         cin >> a >>b;
         cout << a +b;
+        getch();
     }
